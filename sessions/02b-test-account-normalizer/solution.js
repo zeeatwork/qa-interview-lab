e7485f562ac8581f5a414f181c9136fde02e75a1
zeeatwork <zeeathome935@gmail.com>
2026-09-13 20:34:46 -0400

    Save completed Session 2B

--- sessions/02b-test-account-normalizer/solution.js
+++ sessions/02b-test-account-normalizer/solution.js
@@ -6,19 +6,22 @@
  */
 export function prepareTestAccounts(accounts) {
     let filteredArray = JSON.parse(JSON.stringify(accounts));
-    filteredArray.forEach((filteredAccount, i) => {
-      if(filteredAccount.enabled !== true || filteredAccount.type != 'automation'){
-        filteredArray.splice(i, 1);
-      }
-       });
-       if(!filteredArray){
-        return filteredArray;
-      } else {
+    for (let i = filteredArray.length - 1; i >= 0; i--) {
+    const filteredAccount = filteredArray[i];
+
+  if (
+    filteredAccount.enabled !== true ||
+    filteredAccount.type !== "automation"
+  ) {
+    filteredArray.splice(i, 1);
+  }
+}
+    
        filteredArray.forEach((filteredAccount) => {
 
         if(!filteredAccount["environment"]){ filteredAccount["environment"] = "staging";
         }
-      })}
+      })
       let preparedAccounts = filteredArray.map(account => ({id: account['id'], email: account["email"].toLowerCase(), environment: account["environment"]}));
   return preparedAccounts;
 }
